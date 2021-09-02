0da7e1274b3563366bd35a9e535754e9d7082507
Ayoade David <adewoleayoade057@gmail.com>
2021-09-01 19:26:53 -0700

    Add ImageUploader

--- TextMarkup.js
+++ TextMarkup.js
@@ -1,6 +1,6 @@
 function TextMarkup(content, elementattr={}){
     elementattr = 
-    {h2:"",h3:"",strong:"",em:"",u:"",mark:"",q:"",...elementattr};
+    {h2:"",h3:"",strong:"",em:"",u:"",mark:"",q:"",img:"",...elementattr};
     Context = this;
     let body = DOMHelp._("body");
     let markupboard = this.board = 
@@ -8,20 +8,20 @@ function TextMarkup(content, elementattr={}){
     body.append(markupboard);
     markupboard.innerHTML = 
     "<div id='TM-buttonsboard'><div id=\"TM-scrollpageX1\">"
-        +"<button data-mark='formatBlock:<h2 "+elementattr.h2+">:</h2><p>' >To Major Heading </button>"
-        +"<button data-mark='formatBlock:<h3 "+elementattr.h3+">:</h3><p> ' >To Sub-heading </button>"
-        +"<button data-mark='formatBlock:<strong "+elementattr.strong+">:</strong>' id='toB'> Bold</button>"
-        +"<button data-mark='formatBlock:<em "+elementattr.em+">:</em>' >Italizied </button>"
-        +"<button data-mark='formatBlock:<u "+elementattr.u+">:</u>' >     Underline</button>"
-        +"<button data-mark='formatBlock:<mark "+elementattr.mark+">:</mark>' >           Highlight</button>"
+        +"<button data-mark='formatBlock:<h2 "+elementattr.h2+">:</h2><p>' > <strong>Tt</strong> </button>"
+        +"<button data-mark='formatBlock:<h3 "+elementattr.h3+">:</h3><p> ' >  <strong>Ttt</strong> </button>"
+        +"<button data-mark='formatBlock:<strong "+elementattr.strong+">:</strong>' id='toB'>  <strong>B</strong> </button>"
+        +"<button data-mark='formatBlock:<em "+elementattr.em+">:</em>' > <em>i</em> </button>"
+        +"<button data-mark='formatBlock:<u "+elementattr.u+">:</u>' >       <u>U</u></button>"
+        +"<button data-mark='formatBlock:<mark "+elementattr.mark+">:</mark>' > <mark>Highlight</mark> </button>"
         +"<button data-mark='formatBlock:<q "+elementattr.q+" cite=\":\"></q>' >Quote</button>"
         +"<button id='TM-undo-but'>Undo Action</button>"
         +"<button id='TM-hideboard-but'>Hide Board</button>"
-        +"<button id='TM-clearall-but'>Clear All</button>"
+        +"<button id='TM-clearall-but'>x</button>"
     +"</div>"
     +"<div id=\"TM-scrollpageX2\">"
         +"<button data-mark='formatBlock:<ul blogul><li>:</li></ul>'  type=\"createspace\">Create List</button>"
-        +"<button data-mark=\"formatBlock:<img blogimg class='blogims' src='img/defaultimlast.png' alt=':'></img>\" >Upload Image        </button>"
+        +"<button data-mark='formatBlock:<img tm-img src=\"img/defaultimlast.png\" "+elementattr.img+" alt=\":\"></img>' > Upload Image        </button>"
     +"</div>"
     
     
@@ -32,7 +32,7 @@ function TextMarkup(content, elementattr={}){
    DOMHelp._("#TM-scrollpageX2").style = "width: 100%;     display: inline-block;    white-space: normal;"; 
    [...document.querySelectorAll("#TM-buttonsboard button[data-mark]")]
    .map((but)=>{
-       but.style="background: rgb(22, 19, 55); border-color: transparent;    color: white; font-family: courier;     box-shadow: 1px 1px 5px -1px black; display: inline; margin: 1px;";
+       but.style="background: rgb(22, 19, 55); border-color: transparent;    color: white; font-family: courier;     box-shadow: 1px 1px 5px -1px black; display: inline; margin: 3px;  padding: 5px;";
    });
    
    
@@ -100,8 +100,7 @@ function mark(el, event){
  Context.html = content.innerHTML;
  markactions.push({result: content.innerHTML});
  undoCount = markactions.length-1;
- //add listeners to image  
- //setImagesByClass("blogims");
+ AttachImages();
 }//EO mark
 
 
@@ -141,6 +140,45 @@ function ClearAll(){
     
     
     
+    
+    
+    
+
+this.getImages = function (){
+    var images = [];
+    [...document.querySelectorAll("[tm-img]")].map((tmimg)=>{
+        images.push(tmimg.file);
+    })
+    return images;
+}
+function AttachImages(){
+    [...document.querySelectorAll("[tm-img]")].map((tmimg)=>{
+        new ImageUploader(tmimg);
+    })
+}
+    
+function ImageUploader(image){
+    let image_input = document.createElement("input");
+    image_input.setAttribute("hidden", "");
+    image_input.setAttribute("type", "file");
+    image_input.setAttribute("accept", "image/*");
+    image.onclick = ()=>{ image_input.click();}
+    image_input.onchange = (evt)=>{ 
+        var files = evt.target.files;
+        if(files.length === 0){ alert('No files selected'); return; }
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            image.src = event.target.result;
+        }
+        reader.readAsDataURL(files[0]);
+        image.file = files[0];
+    } //EO product_image
+}//EO ImageUploader
+   
+   
+   
+   
+   
    
    
 }//EO TextMarkUp
